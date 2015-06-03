b97d236dd40c7f52cbda1cb07fe0e08cbe96141e
Daniel Stieber <dstieber@cs.csubak.edu>
2015-06-03 15:33:27 -0700

    fixed background tearing

--- danielS.cpp
+++ danielS.cpp
@@ -43,10 +43,10 @@ void renderSky(Game *game) {
 	glBindTexture(GL_TEXTURE_2D, skyTexture);
 	glBegin(GL_QUADS);
 	int ybottom = game->altitude - yres;
-	glTexCoord2f(0.0f, 1.0f); glVertex2i(0, ybottom);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(0, ybottom - 10);
 	glTexCoord2f(0.0f, 0.0f); glVertex2i(0, game->altitude);
 	glTexCoord2f(1.0f, 0.0f); glVertex2i(xres, game->altitude);
-	glTexCoord2f(1.0f, 1.0f); glVertex2i(xres, ybottom);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i(xres, ybottom - 10);
 	glEnd();
 }
 
